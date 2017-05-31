5be03f1133b7c5b79f768c90ccade52804d6a0ce
Jerome Devost <jdevost@coveo.com>
2017-05-31 17:15:23 -0400

    Change example to use blog.coveo.com Added comments Rework getNodes() to reduce complexity

--- misc/Webscraper.user.js
+++ misc/Webscraper.user.js
@@ -6,7 +6,7 @@
 // @author       Jerome Devost
 // @grant        GM_addStyle
 //
-// @match        http://source.coveo.com/*
+// @match        http://blog.coveo.com/*
 //
 // ==/UserScript==
 
@@ -23,9 +23,9 @@ let jsonRules = [{
       "urls":[".*"]
   },
   "exclude": [{
-    "type": "CSS", "path": "body > header"
+    "type": "CSS", "path": "body > header, iframe"
     }, {
-    "type": "CSS", "path": ".main .wrapper .container aside, .main-content .featured-post .addthis_toolbox"
+    "type": "CSS", "path": ".main .wrapper .container aside, .main-content .featured-post .addthis_toolbox, div#disqus_thread"
     }, {
     "type": "XPATH", "path": "/html/body/footer"
     }, {
@@ -34,20 +34,24 @@ let jsonRules = [{
     "type": "CSS", "path": "div#posts div#placement-bottom, div#posts div#no-posts, div#posts div#form, div#posts div#footer, div#disqus_thread #dsq-comments .pingback"
   }],
   "metadata": {
+    "author": {
+      "type": "XPATH", "path": "/html/body//article//div[@class='meta-info']//div[@class='author-container']/span[@class='author']/a/text()"
+    },
+    "authorloginname": {
+      "type": "XPATH", "path": "substring-before(substring-after(/html/body//article//div[@class='meta-info']//div[@class='author-container']/span[@class='author']/a/@href,'/author/'),'/')"
+    },
     "date": {
-      "type": "XPATH",
-      "path": "substring-before(//meta[@property='article:published_time']/@content,'T')"
+      "type": "XPATH", "path": "/html/body//article//div[@class='meta-info']//div[@class='time-container']/time//@datetime"
     },
     "tags": {
-      "type": "XPATH",
-      "path": "//meta[@property='article:tag']/@content"
+      "type": "XPATH", "path": "/html/body//article//div[@class='topics']/ul/li/a/text()"
     },
     "title": {
-      "type": "XPATH",
-      "path": "/html/body//h1[@class='title']/text()"
+      "type": "XPATH", "path": "/html/body//div[@class='title-container']/h2/a/text()"
     }
   }
 }];
+
 
 
 //
@@ -67,6 +71,7 @@ let jsonRules = [{
 GM_addStyle(`
 #webscraping-container { position: absolute; font-family: sans-serif; right: 0; width: 300px; top: 0; bottom: 0; background-color: #F1F1E1; overflow: auto; font-size: 14px; display: flex; flex-direction: column; }
 #webscraping-container i { color: #F58020; margin: 5px; }
+#webscraping-container input { -webkit-appearance: checkbox !important; height: auto; width: auto; }
 #webscraping-container button { margin: 3px 5px; }
 #webscraping-config { margin: 5px; flex: 4; }
 #webscraping-fields { color: #313A45; overflow: auto; padding: 5px; flex: 6; }
@@ -75,13 +80,6 @@ GM_addStyle(`
 html { overflow: hidden; }
 body { position: absolute; right: 300px; left: 300px; top: 0; bottom: 0; }
 `);
-
-// Helper function to update CSS style on a nodem (n) using a json object (s).
-let setStyle = (n,s)=>{
-  for (let k in s) {
-    n.style[k] = s[k];
-  }
-};
 
 jsonRules = JSON.stringify(jsonRules, null, 2);
 
@@ -94,46 +92,77 @@ n.innerHTML = `<i>Web scraper options:</i><textarea id="webscraping-config" plac
   </div>
   <i>Fields:</i>
   <div id="webscraping-fields"></div>`;
-// <button id="webscraping-again-button">Run Again.</button>
+
 document.documentElement.appendChild(n);
 
+/**
+ * Helper function to update CSS style on a node (n) using a json object (s).
+ */
+let setStyle = (n,s)=>{
+  for (let k in s) {
+    n.style[k] = s[k];
+  }
+};
+
+// Update document body so it plays nicely with a new sidebar.
 setStyle(document.documentElement, { overflow: 'hidden' });
 setStyle(document.body, { position: 'absolute', left: 0, right: '300px', top: 0, bottom: 0, overflow: 'auto' });
 
-let getNodes = rule => {
-	if (rule.type === 'CSS') {
-		let p = rule.path, wantText = false;
-		if (/::text/g.test(p)) {
-			wantText = true;
-			p = p.replace(/::text/g, '');
-		}
-		let nodes = document.querySelectorAll(p);
-		if (wantText) {
-			nodes = nodes.length ? nodes[0].textContent : '';
-		}
-		return rule.isBoolean ? (nodes && nodes.length ? true : false) : nodes;
+/**
+ * Use document.querySelectorAll() and CSS rule to find nodes in the page.
+ */
+let getNodesCSS = rule => {
+	let p = rule.path, wantText = false;
+	if (/::text/g.test(p)) {
+		wantText = true;
+		p = p.replace(/::text/g, '');
 	}
-	else if (rule.type === 'XPATH') {
-		let a = [], o, nodes = document.evaluate(rule.path, document);
-		if (nodes.resultType === XPathResult.UNORDERED_NODE_ITERATOR_TYPE || nodes.resultType === XPathResult.ORDERED_NODE_ITERATOR_TYPE) {
-			while ((o = nodes.iterateNext())) {
-				a.push(o);
-			}
-			return a;
+	let nodes = document.querySelectorAll(p);
+	if (wantText) {
+		nodes = nodes.length ? nodes[0].textContent : '';
+	}
+	return rule.isBoolean ? (nodes && nodes.length ? true : false) : nodes;
+};
+
+/**
+ * Use document.evaluate() and XPATH rule to find nodes in the page.
+ */
+let getNodesXPath = rule => {
+	let a = [], o, nodes = document.evaluate(rule.path, document);
+	if (nodes.resultType === XPathResult.UNORDERED_NODE_ITERATOR_TYPE || nodes.resultType === XPathResult.ORDERED_NODE_ITERATOR_TYPE) {
+		while ((o = nodes.iterateNext())) {
+			a.push(o);
 		}
-		else if (nodes.resultType === XPathResult.NUMBER_TYPE) {
-			return [nodes.numberValue];
-		}
-		else if (nodes.resultType === XPathResult.STRING_TYPE) {
-			return [nodes.stringValue];
-		}
-		else if (nodes.resultType === XPathResult.BOOLEAN_TYPE) {
-			return [nodes.booleanValue];
-		}
+		return a;
+	}
+	else if (nodes.resultType === XPathResult.NUMBER_TYPE) {
+		return [nodes.numberValue];
+	}
+	else if (nodes.resultType === XPathResult.STRING_TYPE) {
+		return [nodes.stringValue];
+	}
+	else if (nodes.resultType === XPathResult.BOOLEAN_TYPE) {
+		return [nodes.booleanValue];
 	}
 	return [];
 };
 
+/**
+ * Returns an array of DOM nodes based on a Web Scraper rule.
+ */
+let getNodes = rule => {
+	if (rule.type === 'CSS') {
+		return getNodesCSS(rule);
+	}
+	else if (rule.type === 'XPATH') {
+		return getNodesXPath(rule);
+	}
+	return [];
+};
+
+/**
+ * Hide DOM nodes from the page based on Web Scraper rules passed in.
+ */
 let exclude = (rules) => {
 	rules.forEach(rule => {
 		let nodes = getNodes(rule);
@@ -146,6 +175,9 @@ let exclude = (rules) => {
 	});
 };
 
+/**
+ * Get the fields matching the metadata in the Web Scraper rules and show them in the sidebar.
+ */
 let getFields = meta => {
 	let a = [];
 	for (var field in meta) {
@@ -167,6 +199,9 @@ let getFields = meta => {
 	document.getElementById('webscraping-fields').innerHTML = `${html.join('\n')}`;
 };
 
+/**
+ * Process the page using the `jsonRules`.
+ */
 let scrapePage = () => {
 	try {
 		let c = JSON.parse(document.getElementById('webscraping-config').value);
@@ -178,23 +213,30 @@ let scrapePage = () => {
 		console.log(e);
 	}
 };
-setTimeout(scrapePage, 222);
-//scrapePage();
 
+/**
+ * Quick util function to encode the JSON as a string. Useful for Sitemap sources.
+ */
 let encodeJson = () => {
 	let json = JSON.parse(document.getElementById('webscraping-config').value);
 	document.getElementById('webscraping-config').value = '"' + JSON.stringify(json).replace(/\\/g, '\\\\').replace(/"/g, '\\"') + '"';
-    document.getElementById('webscraping-config').style.display = 'block';
+	document.getElementById('webscraping-config').style.display = 'block';
 };
+/**
+ * Toggle Hide/Remove for the exclude rules. The setting is saved in the localStorage and read again on page reload.
+ */
 let toggleHide = () => {
 	localStorage.setItem('hideWhenExclude', '' + (localStorage.getItem('hideWhenExclude') !== 'true'));
 	document.location = document.location.href;
 };
 
-//document.getElementById('webscraping-again-button').onclick = scrapePage;
+// set up onclick events for the Hide/Remove toggle and Encode button.
 document.getElementById('webscraping-encode-button').onclick = encodeJson;
 document.getElementById('webscraping-hide-cb').onclick = toggleHide;
 if ((localStorage.getItem('hideWhenExclude') === 'true')) {
 	document.getElementById('webscraping-hide-cb').checked = 'checked';
 }
+
+setTimeout(scrapePage, 200); // old school trick to execute in a new 'thread' (not really, but you know what I mean).
+
 })();
