4fc57fc7cfd302abefd0339d2043f4281ec69fd9
malpp <malpp@users.noreply.github.com>
2017-06-05 09:05:37 -0400

    Fixed merge

--- misc/chrome_extension/content-script.js
+++ misc/chrome_extension/content-script.js
@@ -197,11 +197,9 @@ function clickHandler(e) {
 		e.stopImmediatePropagation();
 
 	}
-		sendMouseElement();
+	sendMouseElement();
 
-		return false;
-	}
-
+	return false;
 }
 
 function sendMouseElement() {
@@ -229,57 +227,56 @@ function sendMouseElement() {
 
 //https://stackoverflow.com/questions/9197884/how-do-i-get-the-xpath-of-an-element-in-an-x-html-file
 function getXPath(node) {
-    var comp, comps = [];
-    var parent = null;
-    var xpath = '';
-    var getPos = function(node) {
-        var position = 1, curNode;
-        if (node.nodeType == Node.ATTRIBUTE_NODE) {
-            return null;
-        }
-        for (curNode = node.previousSibling; curNode; curNode = curNode.previousSibling) {
-            if (curNode.nodeName == node.nodeName) {
-                ++position;
-            }
-        }
-        return position;
-     }
+	var comp, comps = [];
+	var parent = null;
+	var xpath = '';
+	var getPos = function (node) {
+		var position = 1, curNode;
+		if (node.nodeType == Node.ATTRIBUTE_NODE) {
+			return null;
+		}
+		for (curNode = node.previousSibling; curNode; curNode = curNode.previousSibling) {
+			if (curNode.nodeName == node.nodeName) {
+				++position;
+			}
+		}
+		return position;
+	}
 
-    if (node instanceof Document) {
-        return '/';
-    }
+	if (node instanceof Document) {
+		return '/';
+	}
 
-    for (; node && !(node instanceof Document); node = node.nodeType == Node.ATTRIBUTE_NODE ? node.ownerElement : node.parentNode) {
-        comp = comps[comps.length] = {};
-        switch (node.nodeType) {
-            case Node.TEXT_NODE:
-                comp.name = 'text()';
-                break;
-            case Node.ATTRIBUTE_NODE:
-                comp.name = '@' + node.nodeName;
-                break;
-            case Node.PROCESSING_INSTRUCTION_NODE:
-                comp.name = 'processing-instruction()';
-                break;
-            case Node.COMMENT_NODE:
-                comp.name = 'comment()';
-                break;
-            case Node.ELEMENT_NODE:
-                comp.name = node.nodeName;
-                break;
-        }
-        comp.position = getPos(node);
-    }
+	for (; node && !(node instanceof Document); node = node.nodeType == Node.ATTRIBUTE_NODE ? node.ownerElement : node.parentNode) {
+		comp = comps[comps.length] = {};
+		switch (node.nodeType) {
+			case Node.TEXT_NODE:
+				comp.name = 'text()';
+				break;
+			case Node.ATTRIBUTE_NODE:
+				comp.name = '@' + node.nodeName;
+				break;
+			case Node.PROCESSING_INSTRUCTION_NODE:
+				comp.name = 'processing-instruction()';
+				break;
+			case Node.COMMENT_NODE:
+				comp.name = 'comment()';
+				break;
+			case Node.ELEMENT_NODE:
+				comp.name = node.nodeName;
+				break;
+		}
+		comp.position = getPos(node);
+	}
 
-    for (var i = comps.length - 1; i >= 0; i--) {
-        comp = comps[i];
-        xpath += '/' + comp.name;
-        if (comp.position != null && comp.position > 1) {
-            xpath += '[' + comp.position + ']';
-        }
-    }
+	for (var i = comps.length - 1; i >= 0; i--) {
+		comp = comps[i];
+		xpath += '/' + comp.name;
+		if (comp.position != null && comp.position > 1) {
+			xpath += '[' + comp.position + ']';
+		}
+	}
 
-    return xpath.toLowerCase();
+	return xpath.toLowerCase();
 
 }
->>>>>>> Added click by mouse
