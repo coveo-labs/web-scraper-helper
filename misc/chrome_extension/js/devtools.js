823598114b9641ca1fb11b4617e5589b4ec45422
Jerome Devost <jdevost@coveo.com>
2017-11-29 12:18:03 -0500

    Put back panel's title

--- misc/chrome_extension/js/devtools.js
+++ misc/chrome_extension/js/devtools.js
@@ -1,6 +1,6 @@
 //Create the tab in the dev tools (Control+Shift+I)
 /*global chrome*/
-chrome.devtools.panels.create("W-S-H",
+chrome.devtools.panels.create("Web Scraping",
     "/images/icon-19.png",
     "/panel-app/build/index.html",
     null
