bc3547d7a646989fdabecb0ce5348067da5ff413
JDevost <jdevost@coveo.com>
2021-01-22 17:56:29 -0500

    update to React 17

--- panel-app/src/Rules.test.js
+++ panel-app/src/Rules.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { configure, mount, render, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import Rules from './Rules';
 
