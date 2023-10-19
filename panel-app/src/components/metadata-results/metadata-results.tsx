b5c456a96387d60b5550123b9140d4d48bb091ca
JDevost <jdevost@coveo.com>
2023-10-19 10:07:42 -0400

    revert changes to prettier config

--- panel-app/src/components/metadata-results/metadata-results.tsx
+++ panel-app/src/components/metadata-results/metadata-results.tsx
@@ -1,48 +1,48 @@
 import { Component, State, h } from '@stencil/core';
 import { getMetadataResults } from '../store';
 @Component({
-  tag: 'metadata-results',
-  styleUrl: 'metadata-results.css',
-  shadow: true,
+	tag: 'metadata-results',
+	styleUrl: 'metadata-results.css',
+	shadow: true,
 })
 export class MetadataResults {
-  @State() results: any;
+	@State() results: any;
 
-  async componentWillRender() {
-    // needs to be fixed
-    this.results = await getMetadataResults();
-    console.log('results', this.results);
-  }
+	async componentWillRender() {
+		// needs to be fixed
+		this.results = await getMetadataResults();
+		console.log('results', this.results);
+	}
 
-  render() {
-    return (
-      <div class="result-container">
-        <table id="resultGlobalTable">
-          <thead>
-            <tr class="table-header">
-              <th>Metadata name</th>
-              <th style={{ textAlign: 'left' }}>Value(s)</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.results &&
-              this.results.length &&
-              this.results.map((item) => {
-                return (
-                  <tr>
-                    <td class="metadata-name-section">{item.name}</td>
-                    <td class="values-section" colSpan={item.values && item.values.length}>
-                      {item.values.length &&
-                        item.values.map((val, idx) => {
-                          return <div key={idx}>{val}</div>;
-                        })}
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div class="result-container">
+				<table id="resultGlobalTable">
+					<thead>
+						<tr class="table-header">
+							<th>Metadata name</th>
+							<th style={{ textAlign: 'left' }}>Value(s)</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.results &&
+							this.results.length &&
+							this.results.map((item) => {
+								return (
+									<tr>
+										<td class="metadata-name-section">{item.name}</td>
+										<td class="values-section" colSpan={item.values && item.values.length}>
+											{item.values.length &&
+												item.values.map((val, idx) => {
+													return <div key={idx}>{val}</div>;
+												})}
+										</td>
+									</tr>
+								);
+							})}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
